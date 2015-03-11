049ee86439525288298810811acbf9fdfdaf1437
Wyam <willy.am.ong@gmail.com>
2015-03-11 15:55:20 +0100

    FS fail to post token to FC -> redirect to homepage

--- serviceProviderNodeJSPassport/helpers/passportAuthenticateWithAcrClaims.js
+++ serviceProviderNodeJSPassport/helpers/passportAuthenticateWithAcrClaims.js
@@ -36,9 +36,9 @@ PassportAuthenticateWithAcrClaims.prototype.authenticate = function(req, options
 
         oauth2.getOAuthAccessToken(code, { grant_type: 'authorization_code', redirect_uri: callbackURL }, function(err, accessToken, refreshToken, params) {
             if (err) {
-                console.error('error when getting access token with FC ' + this._tokenURL);
+                console.error('error when getting access token with FC ' + this._getAccessTokenUrl());
                 console.error(err.stack)
-                return self.fail({redirect_uri: req.session.demandeEnCoursQuery.redirect_uri});
+                return self.fail({redirect_uri: req.session.demandeEnCoursQuery.redirect_uri || '/'});
             }
             var idToken = params['id_token'];
             if (!idToken) {
