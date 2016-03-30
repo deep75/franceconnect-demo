87f2646a4b1fdf65bfac6c2e2de02885a49fabc4
Roxanne Ricci <rricci@octo.com>
2016-03-30 16:42:59 +0200

    [FC-80] Add userinfo on index page

--- serviceProviderNodeJSPassport/controllers/index.js
+++ serviceProviderNodeJSPassport/controllers/index.js
@@ -4,18 +4,18 @@ var IndexController = function(){};
 
 IndexController.prototype.handleMain = function(req, res){
     if(req.session.user){
-        res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user});
+        res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: req.session.userInfo});
     } else {
         if(req.session.passport && req.session.passport.user){
             var given_name = (req.session.passport.user._json.given_name) ? req.session.passport.user._json.given_name : '';
             var family_name = (req.session.passport.user._json.family_name) ? req.session.passport.user._json.family_name : '';
             req.session.user = given_name + ' ' + family_name;
-            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user});
+            var userInfo = (req.session.passport.user._json) ? req.session.passport.user._json : '';
+            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: userInfo});
         } else {
-            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: undefined});
+            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: undefined, userInfo: undefined});
         }
     }
 };
-
 
 module.exports.IndexController = IndexController;
--- serviceProviderNodeJSPassport/test/controllers/indexSpec.js
+++ serviceProviderNodeJSPassport/test/controllers/indexSpec.js
@@ -6,14 +6,20 @@ var controller = new (require('../../controllers/index.js').IndexController)();
 describe('index page (/)', function(){
     it('should render the page using user session data if user info exists in current session', function(){
         //setup
-        var req = {session:{user:'john'}};
+        var req = {
+            session: {
+                user: 'john', userInfo: {
+                    given_name: 'john'
+                }
+            }
+        };
         var res = {render:sinon.spy()};
         //action
         controller.handleMain(req, res);
         //assert
         assert.equal(res.render.calledOnce, true);
         assert.equal(res.render.getCall(0).args[0], 'index');
-        assert.deepEqual(res.render.getCall(0).args[1], {title: 'Démonstrateur France Connect - Accueil', user: 'john'});
+        assert.deepEqual(res.render.getCall(0).args[1], {title: 'Démonstrateur France Connect - Accueil', user: 'john', userInfo: {given_name: 'john'}});
     });
 
     it('should render the page with an "undefined" user data if there is no user data in the session and no passport session either', function(){
@@ -25,7 +31,7 @@ describe('index page (/)', function(){
         //assert
         assert.equal(res.render.calledOnce, true);
         assert.equal(res.render.getCall(0).args[0], 'index');
-        assert.deepEqual(res.render.getCall(0).args[1], {title: 'Démonstrateur France Connect - Accueil', user: undefined});
+        assert.deepEqual(res.render.getCall(0).args[1], {title: 'Démonstrateur France Connect - Accueil', user: undefined, userInfo: undefined});
     });
 
     it('should set req.session.user data before rendering if passport session data is available', function(){
@@ -37,6 +43,9 @@ describe('index page (/)', function(){
         //assert
         assert.equal(res.render.calledOnce, true);
         assert.equal(res.render.getCall(0).args[0], 'index');
-        assert.deepEqual(res.render.getCall(0).args[1], {title: 'Démonstrateur France Connect - Accueil', user: 'John Oliver'});
+        assert.deepEqual(res.render.getCall(0).args[1], {title: 'Démonstrateur France Connect - Accueil',
+            user: 'John Oliver',
+            userInfo: {family_name: 'Oliver', given_name: 'John'}
+        });
     });
 });
